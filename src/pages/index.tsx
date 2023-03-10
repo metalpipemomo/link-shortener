39ad8912d3822fe803e0b456a56571c8f730ff99
MoBaydoun <mbaydoun@my.bcit.ca>
2023-03-09 21:03:40 -0800

    Added NextAuth with Google OAuth Provider. Made a component to log in. Will refactor index.tsx later.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { type NextPage } from 'next';
 import Head from 'next/head';
 import { FormEvent, useRef } from 'react';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 import { api } from '~/utils/api';
 
@@ -112,6 +113,27 @@ const Home: NextPage = () => {
                 </div>
             </main>
         </>
+    );
+};
+
+const AuthShowcase: React.FC = () => {
+    const { data: sessionData } = useSession();
+
+    return (
+        <div className='flex flex-col items-center justify-center gap-4'>
+            <p className='text-center text-2xl text-white'>
+                {sessionData && (
+                    <span>Logged in as {sessionData.user?.name}</span>
+                )}
+            </p>
+            <button
+                className='rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20'
+                onClick={
+                    sessionData ? () => void signOut() : () => void signIn()
+                }>
+                {sessionData ? 'Sign out' : 'Sign in'}
+            </button>
+        </div>
     );
 };
 
