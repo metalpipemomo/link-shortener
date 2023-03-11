d32025c317f962c13eb6d669bad638d84a5bc86f
luminaryFlowers <71102109+luminaryFlowers@users.noreply.github.com>
2023-03-11 02:55:41 -0800

    classname lint plucking in loadingspinner

--- src/pages/components/LoadingSpinner.tsx
+++ src/pages/components/LoadingSpinner.tsx
@@ -13,7 +13,7 @@ const LoadingSpinner: NextPage<Props> = ({ size, color, className }) => {
             width={size}
             height={size}
             fill={color}
-            className={`animate-spin ${className}`}
+            className={`animate-spin ${className as string}`}
             viewBox='0 0 16 16'>
             <path d='M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z' />
             <path
